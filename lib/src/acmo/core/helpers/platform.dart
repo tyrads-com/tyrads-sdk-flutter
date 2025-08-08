283cc37ca17cdbb0ed7442a05ced520a1fbdbdbb
basharatmehdi <basharatmehdidar@gmail.com>
2025-08-08 16:25:02 +0530

    custom platform class created

--- lib/src/acmo/core/helpers/platform.dart
+++ lib/src/acmo/core/helpers/platform.dart
@@ -3,6 +3,18 @@ import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 
+class AcmoPlatform {
+  static bool get isWeb => kIsWeb;
+
+  static bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  static bool get isIOS => !kIsWeb && Platform.isIOS;
+  static bool get isFuchsia => !kIsWeb && Platform.isFuchsia;
+  static bool get isLinux => !kIsWeb && Platform.isLinux;
+  static bool get isMacOS => !kIsWeb && Platform.isMacOS;
+  static bool get isWindows => !kIsWeb && Platform.isWindows;
+}
+
+
 acmoGetPlatformName() {
   var platformName = '';
   if (kIsWeb) {
