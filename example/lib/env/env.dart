994613b5e99c0b933c238e0711a195151193a177
basharatmehdi <basharatmehdidar@gmail.com>
2025-08-14 10:54:13 +0530

    added ios keys

--- example/lib/env/env.dart
+++ example/lib/env/env.dart
@@ -10,6 +10,10 @@ abstract class Env {
     static const String TYRADS_SDK_KEY = _Env.TYRADS_SDK_KEY;
     @EnviedField(varName: 'TYRADS_SDK_SECRET')
     static const String TYRADS_SDK_SECRET = _Env.TYRADS_SDK_SECRET;
+    @EnviedField(varName: 'TYRADS_SDK_IOS_KEY')
+    static const String TYRADS_SDK_IOS_KEY = _Env.TYRADS_SDK_IOS_KEY;
+    @EnviedField(varName: 'TYRADS_SDK_IOS_SECRET')
+    static const String TYRADS_SDK_IOS_SECRET = _Env.TYRADS_SDK_IOS_SECRET;
     @EnviedField(varName: 'TYRADS_SDK_ENC_KEY')
     static const String TYRADS_SDK_ENC_KEY = _Env.TYRADS_SDK_ENC_KEY;
 }
