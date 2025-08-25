8f844a916cb6e7e1fe368875f08129b8e534b991
basharatmehdi <basharatmehdidar@gmail.com>
2025-08-25 16:05:11 +0530

    fixed acmo web image issue on other platforms

--- lib/src/acmo/core/components/web_image_stub.dart
+++ lib/src/acmo/core/components/web_image_stub.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AcmoWebImage extends StatefulWidget {
-  const AcmoWebImage({super.key});
+  const AcmoWebImage({
+    super.key,
+    required this.url,
+    this.width,
+    this.height,
+    this.fit,
+  });
+
+  final String url;
+  final double? width;
+  final double? height;
+  final BoxFit? fit;
 
   @override
   State<AcmoWebImage> createState() => _AcmoWebImageState();
