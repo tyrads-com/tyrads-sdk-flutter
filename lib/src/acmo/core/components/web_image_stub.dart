c1fde887391ad8b10809b7a1b28d8c25607b730e
basharatmehdi <basharatmehdidar@gmail.com>
2025-08-25 16:00:01 +0530

    created custom acmo image that handles web cors error

---
+++ lib/src/acmo/core/components/web_image_stub.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AcmoWebImage extends StatefulWidget {
+  const AcmoWebImage({super.key});
+
+  @override
+  State<AcmoWebImage> createState() => _AcmoWebImageState();
+}
+
+class _AcmoWebImageState extends State<AcmoWebImage> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
