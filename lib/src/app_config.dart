e1b8adb9170d685ea10f7d6c3bc15e3652136e24
basharatmehdi <basharatmehdidar@gmail.com>
2026-01-08 12:59:23 +0530

    sdk version updated for push notifications

--- lib/src/app_config.dart
+++ lib/src/app_config.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 
 class AcmoConfig {
   static const API_VERSION = "3.0";
-  static const BUILD_VERSION = "0";
+  static const BUILD_VERSION = "1";
   static const AV = "4";
-  static const SDK_VERSION = "3.3.0-$BUILD_VERSION";
+  static const SDK_VERSION = "3.4.0-$BUILD_VERSION";
   static const SDK_PLATFORM = "Flutter";
   static const BASE_URL = "https://api.tyrads.com/v$API_VERSION/";
   static const PRIMARY_COLOR =
