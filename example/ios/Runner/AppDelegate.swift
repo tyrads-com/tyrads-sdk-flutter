e52c30aad0a2f4b63bf5073d8cf1e8ae73a83771
basharatmehdi <basharatmehdidar@gmail.com>
2026-01-19 12:54:45 +0530

    remove manual APNS setup from example app

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import tyrads_sdk
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,46 +8,10 @@ import tyrads_sdk
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    APNsAppDelegateProxy.configure()
-    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(
       application,
       didFinishLaunchingWithOptions: launchOptions
     )
   }
-
-  override func application(
-    _ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-  ) {
-    APNsAppDelegateProxy.didRegisterForRemoteNotifications(deviceToken)
-
-    super.application(
-      application,
-      didRegisterForRemoteNotificationsWithDeviceToken: deviceToken
-    )
-  }
-
-  override func application(
-    _ application: UIApplication,
-    didReceiveRemoteNotification userInfo: [AnyHashable : Any],
-    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
-  ) {
-
-    APNsAppDelegateProxy.didReceiveRemoteNotification(
-      userInfo,
-      completionHandler: completionHandler
-    )
-    
-    let wrappedCompletionHandler: (UIBackgroundFetchResult) -> Void = { result in
-    }
-    
-    super.application(
-      application,
-      didReceiveRemoteNotification: userInfo,
-      fetchCompletionHandler: wrappedCompletionHandler
-    )
-  }
-
 }
