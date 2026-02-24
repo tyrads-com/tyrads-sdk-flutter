4e93de390b4d1be5a763db14515c887447b51e56
naveedbhat <naveed.bhat@acmo.in>
2026-02-24 12:03:44 +0530

    implement key obfuscation for Firebase configuration

--- lib/src/acmo/core/services/notifications/firebase_config.dart
+++ lib/src/acmo/core/services/notifications/firebase_config.dart
@@ -1,16 +1,20 @@
-import 'package:tyrads_sdk/src/acmo/core/helpers/platform.dart';
+import 'package:tyrads_sdk/src/acmo/core/helpers/security_utils.dart';
 
 class FirebaseConfig {
-  static const String projectId = 'tyrsdk-32c6e';
-  static const String appIdiOS = '1:365439351599:ios:f87d6ef553487fd2b87fda';
-  static const String appIdAndroid = '1:365439351599:android:f4d793b204e29feab87fda';
-  static String appId = AcmoPlatform.isAndroid ? appIdAndroid : appIdiOS;
-  static const String apiKeyiOS = 'AIzaSyAlCr4bs4x5aiKOVzPrQMfhcRgpBVBkPOA';
-  static const String apiKeyAndroid = 'AIzaSyCD_nKOf9iDluRLi4El9XU3joaCzlU9zgc';
-  static String apiKey = AcmoPlatform.isAndroid ? apiKeyAndroid : apiKeyiOS;
-  static const String messagingSenderId = '365439351599';
-  static const String storageBucket = 'tyrsdk-32c6e.firebasestorage.app';
 
+  static const pId = [71, 75, 71, 65, 0, 8, 72, 3, 86, 6, 4, 92];
+  static const aId = [2, 8, 6, 4, 81, 87, 86, 9, 87, 80, 3, 12, 15, 91, 2, 86, 93, 86, 71, 93, 13, 7, 95, 81, 5, 4, 1, 11, 3, 80, 92, 84, 86, 2, 81, 87, 86, 90, 83, 82, 92, 82, 84, 93, 87];
+  static const kId = [114, 123, 79, 83, 55, 26, 36, 95, 61, 6, 94, 82, 111, 16, 114, 15, 3, 99, 71, 68, 54, 15, 19, 64, 39, 33, 115, 115, 101, 4, 11, 111, 71, 101, 69, 64, 20, 85, 85];
+  static const sId = [0, 4, 0, 6, 87, 90, 86, 5, 85, 80, 11, 0];
+  static const bId = [71, 75, 71, 65, 0, 8, 72, 3, 86, 6, 4, 92, 24, 4, 81, 69, 86, 80, 84, 65, 1, 16, 17, 95, 22, 4, 85, 92, 24, 3, 72, 71];
+
+
+  static String get projectId  => SecurityUtils.deobfuscate(pId);
+  static String get appId => SecurityUtils.deobfuscate(aId);
+  static String get apiKey => SecurityUtils.deobfuscate(kId);
+  static String get messagingSenderId => SecurityUtils.deobfuscate(sId);
+  static String get storageBucket => SecurityUtils.deobfuscate(bId);
+  
   static Map<String, dynamic> toMap() {
     return {
       'projectId': projectId,
