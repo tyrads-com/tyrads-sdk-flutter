3ab7474056102bb417c9aa81d51958db64d3debb
basharatmehdi <basharatmehdidar@gmail.com>
2026-03-05 15:15:31 +0530

    update API version and sdk version to 4.0

--- lib/src/app_config.dart
+++ lib/src/app_config.dart
@@ -3,10 +3,10 @@
 import 'package:flutter/material.dart';
 
 class AcmoConfig {
-  static const API_VERSION = "3.0";
+  static const API_VERSION = "4.0";
   static const BUILD_VERSION = "1";
   static const AV = "4";
-  static const SDK_VERSION = "3.4.0-$BUILD_VERSION";
+  static const SDK_VERSION = "4.0.0-$BUILD_VERSION";
   static const SDK_PLATFORM = "Flutter";
   static const BASE_URL = "https://api.tyrads.com/v$API_VERSION/";
   static const PRIMARY_COLOR =
